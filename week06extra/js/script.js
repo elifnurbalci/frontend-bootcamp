1d6a3f0fc359c1902ae2a250d291fc9b5d2f7ff7
elifnurbalci <elfnurbalci@gmail.com>
2024-07-16 21:57:58 +0100

    show time

---
+++ week06extra/js/script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.drum').forEach(button => {
+    button.addEventListener('click', () => playSound(button));
+});
+
+document.addEventListener('keydown', event => {
+    const key = event.key.toUpperCase();
+    const button = document.querySelector(`.drum:contains(${key})`);
+    if (button) playSound(button);
+});
+
+function playSound(button) {
+    const sound = new Audio(button.getAttribute('data-sound'));
+    sound.play();
+    button.classList.add('playing');
+    setTimeout(() => button.classList.remove('playing'), 200);
+}
