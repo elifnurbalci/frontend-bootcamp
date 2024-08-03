10b2f765304d2fd156350cf54fa13fa00e704a6c
elifnurbalci <elfnurbalci@gmail.com>
2024-08-03 19:59:42 +0100

    responsive yapildi, nav-tabmenu-header scroll icin js ler eklendi

--- week08/js/main.js
+++ week08/js/main.js
@@ -1,3 +1,4 @@
+//header scroll islemi
 (function () {
     'use strict';
     var header = document.getElementById('header');
@@ -11,3 +12,33 @@
     window.addEventListener('load', headerScrolledHandler);
     document.addEventListener('scroll', headerScrolledHandler);
 })();
+
+
+//classes tab menu fonksiyonu
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".tab-button");
+    const contents = document.querySelectorAll(".class-content");
+
+    buttons.forEach((button, index) => {
+        button.addEventListener("click", function () {
+            buttons.forEach(btn => btn.classList.remove("active"));
+            contents.forEach(content => content.classList.remove("active"));
+
+            button.classList.add("active");
+            contents[index].classList.add("active");
+        });
+    });
+
+    buttons[0].classList.add("active");
+    contents[0].classList.add("active");
+});
+
+//nav bar --> hamburger menu
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navbar = document.getElementById("navbar");
+
+    menuToggle.addEventListener("click", function () {
+        navbar.style.display = navbar.style.display === "flex" ? "none" : "flex";
+    });
+});
