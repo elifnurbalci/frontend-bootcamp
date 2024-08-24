77154141de686a5abc594f4c2c7689433c8e981c
elifnurbalci <elfnurbalci@gmail.com>
2024-08-24 19:44:58 +0100

    soru siklarinin timeout u eklendi

--- week12-quiz-app/src/components/Questions/Questions.jsx
+++ week12-quiz-app/src/components/Questions/Questions.jsx
@@ -3,33 +3,49 @@ import questions from '/public/questions.js'
 import { useState, useEffect } from 'react';
 
 function Questions() {
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setCurrentQuestionIndex((prevIndex) =>
-          prevIndex === questions.length - 1 ? 0 : prevIndex + 1
-        );
-      }, 30000); // Her 30 saniyede bir soru değişir
-  
-      return () => clearInterval(interval);
-    }, []);
-  
-    const currentQuestion = questions[currentQuestionIndex];
-  
-    return (
-      <div className="questions">
-        <div className="question-block">
-          <p>{currentQuestionIndex + 1}. {currentQuestion.question}</p>
-          <img src={currentQuestion.media} alt="media" style={{ width: '200px' }} />
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [showOptions, setShowOptions] = useState(false);
+
+  useEffect(() => {
+    const questionInterval = setInterval(() => {
+      setCurrentQuestionIndex((prevIndex) =>
+        prevIndex === questions.length - 1 ? 0 : prevIndex + 1
+      );
+      setShowOptions(false); 
+    }, 30000);
+
+    return () => clearInterval(questionInterval);
+  }, []);
+
+  useEffect(() => {
+    const optionTimeout = setTimeout(() => {
+      setShowOptions(true);
+    }, 4000);
+
+    return () => clearTimeout(optionTimeout);
+  }, [currentQuestionIndex]);
+
+  const currentQuestion = questions[currentQuestionIndex];
+
+  return (
+    <div className="questions">
+      <div className="question-block">
+        <h2>{currentQuestionIndex + 1}. Question</h2>
+        <p>{currentQuestion.question}</p>
+        <img src={currentQuestion.media} alt="media" style={{ width: '200px' }} />
+        
+        {showOptions ? (
           <ul>
             {currentQuestion.options.map((option, optionIndex) => (
               <li key={optionIndex}>{option}</li>
             ))}
           </ul>
-        </div>
+        ) : (
+          <p>......</p>
+        )}
       </div>
-    );
-  }
-  
-  export default Questions;
+    </div>
+  );
+}
+
+export default Questions;
