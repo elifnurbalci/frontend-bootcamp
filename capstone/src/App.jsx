1e84e959715aa79b1d4b7f5c574bd6f986a3850f
elifnurbalci <elfnurbalci@gmail.com>
2024-09-18 18:13:40 +0100

    added welcome page

--- capstone/src/App.jsx
+++ capstone/src/App.jsx
@@ -1,11 +1,22 @@
 
 import './App.css'
+import Author from './pages/Author'
+import Book from './pages/Book'
+import Borrowing from './pages/Borrowing'
+import Category from './pages/Category'
+import Publisher from './pages/Publisher'
+import Welcome from './pages/Welcome'
 
 function App() {
 
   return (
     <>
-
+      <Welcome />
+      <Publisher />
+      <Category />
+      <Borrowing />
+      <Book />
+      <Author />
     </>
   )
 }
