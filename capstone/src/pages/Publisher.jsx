e4d53c241b55364a08c4601c23ea2bef090c284e
elifnurbalci <elfnurbalci@gmail.com>
2024-09-24 23:04:40 +0100

    added publisher

--- capstone/src/pages/Publisher.jsx
+++ capstone/src/pages/Publisher.jsx
@@ -1,53 +1,126 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+
 
 function Publisher() {
     const initialPublisher = {
-        "id": 0,
-        "name": "string",
-        "establishmentYear": 0,
-        "address": "string"
+        id: "",
+        name: "",
+        establishmentYear: "",
+        address: "",
     };
 
+    const [newPublisher, setNewPublisher] = useState(initialPublisher);
+    const [updatedPublisher, setUpdatedPublisher] = useState(initialPublisher);
     const [publishers, setPublishers] = useState([]);
-    const [publisher, setPublisher] = useState(initialPublisher);
+    const [update, setUpdate] = useState(false);
+    const [isNew, setIsNew] = useState(true);
+
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.get(import.meta.env.VITE_APP_BASE_URL + "/api/v1/publishers")
         .then((res) => {
             setPublishers(res.data);
+            setUpdate(true);
             setLoading(false);
         });
-    }, []);
-    console.log(publishers);
+    }, [update]);
 
     if (loading) {
         return <div>Loading........</div>
     }
 
     const handleAddPublisher = () => {
-        axios.post(import.meta.env.VITE_APP_BASE_URL + "/api/v1/publishers", publisher)
+        axios.post(import.meta.env.VITE_APP_BASE_URL + "/api/v1/publishers", newPublisher)
         .then((res) => {
-            console.log(res);
+            setUpdate(false);
+            setNewPublisher(initialPublisher);
         })
         .catch((error) => {
             console.log(error);
         })
     }
 
-  return (
-    <>
-    <h1>Publisher List</h1>
-    {publishers?.map((publisher, index) => (
-        <div key={index}>
-            <p>
-                {publisher.name}
-            </p>
-        </div>
-    ))}
-    <button onClick={handleAddPublisher}></button>
-    </>
+    const handleUpdatePublisher = () => {
+        axios.put(import.meta.env.VITE_APP_BASE_URL + "/api/v1/publishers/" + updatedPublisher.id, updatedPublisher)
+        .then(() => {
+            setUpdate(false);
+            setIsNew(false);
+            setUpdatedPublisher(initialPublisher);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    const handleDeletePublisher = (publisher) => {
+        axios.delete(import.meta.env.VITE_APP_BASE_URL + "/api/v1/publishers/" + publisher.id)
+        .then(() => {
+            setUpdate(false);
+            setNewPublisher(initialPublisher);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewPublisher((prev) => ({
+        ...prev,
+        [name]: value,
+        }));
+    };
+
+    const handleEditInput = (publisher) => {
+        setUpdatedPublisher(publisher);
+        setIsNew(false);
+    }
+
+    const handleEditInputChange = (e) => {
+        const { name, value } = e.target;
+        setUpdatedPublisher((prev) => ({
+        ...prev,
+        [name]: value,
+        }));
+    };
+
+    return (
+        <>
+        <Box
+        component="form"
+        sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+        noValidate
+        autoComplete="off"
+        >
+        <TextField id="outlined-basic" label="ID" variant="outlined" name="id" value={isNew ? newPublisher.id : updatedPublisher.id} onChange={isNew ? handleInputChange : handleEditInputChange}/>
+        <TextField id="outlined-basic" label="Name" variant="outlined" name="name" value={isNew ? newPublisher.name : updatedPublisher.name} onChange={isNew ? handleInputChange : handleEditInputChange}/>
+        <TextField id="outlined-basic" label="Year" variant="outlined" name="establishmentYear" value={isNew ? newPublisher.establishmentYear : updatedPublisher.establishmentYear} onChange={isNew ? handleInputChange : handleEditInputChange}/>
+        <TextField id="outlined-basic" label="Address" variant="outlined" name="address" value={isNew ? newPublisher.address : updatedPublisher.address} onChange={isNew ? handleInputChange : handleEditInputChange}/>
+        </Box>
+        <h1>Publisher List</h1>
+        {publishers?.map((publisher, index) => (
+            <div key={index}>
+                <div>
+                    <EditNoteIcon onClick={() => handleEditInput(publisher)} />
+                    {publisher.name}
+                    <HighlightOffIcon onClick={() => handleDeletePublisher(publisher)} />
+                    </div>
+                    <br />
+                </div>
+        ))}
+        {isNew ? (
+            <Button variant="contained" onClick={handleAddPublisher}>SAVE</Button>
+        ) : (
+            <Button variant="contained" onClick={handleUpdatePublisher}>UPDATE</Button>
+            )}
+        </>
   )
 }
 export default Publisher
