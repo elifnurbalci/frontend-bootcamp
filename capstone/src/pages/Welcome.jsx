154d3d731b296c63f998bf0de8e75dca1f9917a2
elifnurbalci <elfnurbalci@gmail.com>
2024-09-25 00:31:35 +0100

    added welcome page hero section

--- capstone/src/pages/Welcome.jsx
+++ capstone/src/pages/Welcome.jsx
@@ -1,8 +1,19 @@
+import Button from '@mui/material/Button';
+import { NavLink } from 'react-router-dom';
+import * as React from 'react';
+
 function Welcome() {
   return (
-    <div>
+    <div className='hero'>
       <img src="../../images/welcome.jpg" alt="welcome" />
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, beatae accusantium nulla quibusdam saepe illum eligendi tempora facilis minima eveniet molestiae numquam ipsum quae sapiente fugiat officia cumque magni dolorem! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, beatae accusantium nulla quibusdam saepe illum eligendi tempora facilis minima eveniet molestiae numquam ipsum quae sapiente fugiat officia cumque magni dolorem!</p>
+      <NavLink to="/book" style={{ textDecoration: 'none' }}>
+        <Button variant="contained" style={{ color: 'white' }}>
+          Search Book
+        </Button>
+      </NavLink>
     </div>
-  )
+  );
 }
-export default Welcome
+
+export default Welcome;
